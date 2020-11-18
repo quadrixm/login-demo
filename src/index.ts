f8600b3a67268989d9b1e7a731565c5a46adf608
Mohammad Quadri <quadrixm@gmail.com>
2020-11-18 18:55:51 +0530

    changing port to 3001 in order to run react on 3000

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import {ApplicationConfig} from '@loopback/core';
 export {LoginDemoApplication};
 
 export async function main(options: ApplicationConfig = {}) {
+  if (options.rest) {
+    options.rest.port = 3001;
+    options.rest.host = "0.0.0.0";
+  } else {
+    options.rest = {port: 3001, host: "0.0.0.0"}
+  }
   const app = new LoginDemoApplication(options);
   await app.boot();
   await app.start();
